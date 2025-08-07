1b2e95017949d041a0d5bddb507820ad28fb54d9
momo <mohssinem32@gmail.com>
2025-08-07 15:41:30 +0100

    button probleme fix

--- Frontend/src/components/Navbar.tsx
+++ Frontend/src/components/Navbar.tsx
@@ -9,7 +9,7 @@ function Navbar() {
                 </div>
                 <div className="  flex justify-center items-center gap-3 ">
                      <button className=" h-[50px] w-[170px] border-b-4 border-transparent    hover:border-[#822BD1] transition duration-300 font-bold"><Link to="/">Trouver un entreprise</Link></button>
-                        <button className=" h-[50px] w-[120px] border-b-4 border-transparent hover:border-[#822BD1] transition duration-300 font-bold"><Link to="/">Trouver un job</Link></button>
+                        <button className=" h-[50px] w-[120px] border-b-4 border-transparent hover:border-[#822BD1] transition duration-300 font-bold"><Link to="/jobpage">Trouver un job</Link></button>
                         <button className=" h-[50px] w-[120px] border-b-4 border-transparent hover:border-[#822BD1] transition duration-300 font-bold"><Link to="/">Home</Link></button>
                
                     <button className="bg-gray-100 h-[50px] w-[80px] rounded-lg border-black border-[1px] h-[50px] w-[80px] font-bold"><Link to="/login">Login</Link></button>
