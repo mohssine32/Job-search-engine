83459145dc4373d3ff86055c62cc7bc5b2e4d911
momo <mohssinem32@gmail.com>
2025-10-23 15:12:06 +0200

    modifier les fichier typescript

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -1,6 +1,10 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+
 
 // https://vite.dev/config/
 export default defineConfig({
